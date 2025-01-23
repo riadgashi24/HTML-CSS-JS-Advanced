31edff0785cc7fda9d842b7b15225f2c447f27b7
riadgashi24 <gashiriad09@gmail.com>
2025-01-23 15:13:37 +0000

    part 2

--- moduli30/main.js
+++ moduli30/main.js
@@ -11,16 +11,42 @@
 // for (let i = 0; i < a.length; i++) {
 //     console.log(a[i]);}
     
-const input = document.getElementById("input");
-const btn = document.getElementById("button");
-const output = document.getElementById("h1");
+// const input = document.getElementById("input");
+// const btn = document.getElementById("button");
+// const output = document.getElementById("h1");
 
-btn.onclick = function(){
-    if (input.value > 10) {
-        output.innerHTML = "The value is greater than 10";
-    } else if(input.value < 10) {
-        output.innerHTML = "The value is less than 10";
-    }else{
-        output.innerHTML = "The value is equal to 10";
-    }
+// btn.onclick = function(){
+//     if (input.value > 10) {
+//         output.innerHTML = "The value is greater than 10";
+//     } else if(input.value < 10) {
+//         output.innerHTML = "The value is less than 10";
+//     }else{
+//         output.innerHTML = "The value is equal to 10";
+//     }
+// }
+
+function showCurrentTime(){
+    var clock = document.getElementById('clock');
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    //var noon = currentTime.getDay();
+
+    var meridian = "AM";
+
+    // if (hours >= noon){
+    //     meridian = "PM"
+    // }
+
+    var clockTime = `${hours}:${minutes}:${seconds} ${meridian}`;
+    clock.innerText = clockTime;
+}
+
+var oneSecond = 1000;
+setInterval(showCurrentTime, oneSecond);
+
+function changeImage(){
+    var time = new Date().getHours();
+
 }
